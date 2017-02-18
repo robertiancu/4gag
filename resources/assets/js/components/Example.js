fa314349e25be069884a095cb6028943b2f7b82f
Iancu Robert <robertiancu@protonmail.com>
2017-02-18 19:33:53 +0200

    Create webkit.config for React

---
+++ resources/assets/js/components/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Example extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <h1>It is time to go</h1>
+        );
+    }
+}
